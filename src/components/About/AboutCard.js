0d282c4cf88c16ea27fdaae061405fc16b4cb6c7
TesfawTsion Shimelis <bebishuteda@gmail.com>
2022-12-26 17:02:05 +0300

    changes

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,7 +9,7 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am{" "}
-            <span className="purple">Tesfatsion Shemeles </span>
+            <span className="purple">Tesfawtsion Shemeles </span>
             from <span className="purple"> Gelan!!!</span>
             <br />I am a junior pursuing IMSC in Maths and Computing in BIT
             Mesra.
@@ -32,7 +32,7 @@ function AboutCard() {
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Tesfatsion</footer>
+          <footer className="blockquote-footer">Tesfawtsion</footer>
         </blockquote>
       </Card.Body>
     </Card>

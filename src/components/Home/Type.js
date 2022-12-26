2c34c31fe307479ddcd09b841c6074e2c2a18d8f
Tesfawtsion Shimelis <bebishuteda@gmail.com>
2022-12-26 16:53:35 +0300

    first

---
+++ src/components/Home/Type.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+
+function Type() {
+  return (
+    <Typewriter
+      options={{
+        strings: [
+          "Developer",
+          "Mobile application developer",
+          "Open Source Contributor",
+        ],
+        autoStart: true,
+        loop: true,
+        deleteSpeed: 50,
+      }}
+    />
+  );
+}
+
+export default Type;
